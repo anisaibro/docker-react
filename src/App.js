fae3d8846cd9cf40511f051b0e8997c9f51e0840
Ndrekaj Anisa <ndrekaj@ALB417P21S.local>
2022-03-28 16:09:14 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Anisa David Ndrekaj 2020
         </p>
         <a
           className="App-link"
